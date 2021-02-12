53c001640074712ea9f30ead297be6598fcb670b
lillywillow <pinkhairedsheep@gmail.com>
2021-02-12 14:34:21 +0100

    finished scene detection and extraction

--- description-extraction-demo.py
+++ description-extraction-demo.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     files = glob.glob(path)
 
     result = str()
+    dict = {}
 
     for name in files:
         print(name)
@@ -15,13 +16,27 @@ if __name__ == "__main__":
         # opens the source file
 
             dfcol = pd.read_html(f)
+            result += "<scene>"
             for df in dfcol:
                 listoflists = df.values.tolist()
                 for line in listoflists:
                     if str(line[0]).strip() == "nan":
-                        result += "\n\n<scene>\n"
-                        result += line[1]
-                        result += "\n</scene>"
+                        if (re.match(r'(.*)?[Ss]cene(.*)?', line[1]) or re.match(r'(.*)?[Cc]amera(.*)?', line[1]) or re.match(r'(.*)?[Cc]ut(s)? (back )?to(.*)?', line[1])):
+                            result += "\n</scene>"
+                            result += "\n\n<scene>\n"
+                            result += line[1]
+                        else:
+                            result += "\n"
+                            result += line[1]
+                    else:
+                        if len(line[0]) <= 30:
+                            line[1] = re.sub(r'\[[^\]]*\]', "", line[1])
+                            if line[0] not in dict.keys():
+                                dict[line[0]] = 0
+                            dict[line[0]] += len(line[1].strip().split(" "))
+                        else:
+                            listoflists.remove(line)
+                        result += "\n" + line[0] + ": " + line[1]
             
             nameList = name.split("HTML/")
             newName = nameList[0] + "scenes.txt"
