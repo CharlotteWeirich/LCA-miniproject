41504cf4c525749b3348e8af8179de240d54a70c
lillywillow <pinkhairedsheep@gmail.com>
2021-02-15 18:37:43 +0100

    updated scenes formatting

--- description-extraction-demo.py
+++ description-extraction-demo.py
@@ -16,18 +16,18 @@ if __name__ == "__main__":
         # opens the source file
 
             dfcol = pd.read_html(f)
-            result += "<scene>"
+            result += "<scene>\n"
             for df in dfcol:
                 listoflists = df.values.tolist()
                 for line in listoflists:
                     if str(line[0]).strip() == "nan":
                         if (re.match(r'(.*)?[Ss]cene(.*)?', line[1]) or re.match(r'(.*)?[Cc]amera(.*)?', line[1]) or re.match(r'(.*)?[Cc]ut(s)? (back )?to(.*)?', line[1])):
-                            result += "\n</scene>"
-                            result += "\n\n<scene>\n"
-                            result += line[1]
+                            result += "</scene>\n"
+                            result += "\n<scene>\n"
+                            #result += line[1]
                         else:
                             result += "\n"
-                            result += line[1]
+                            #result += line[1]
                     else:
                         if len(line[0]) <= 30:
                             line[1] = re.sub(r'\[[^\]]*\]', "", line[1])
@@ -36,7 +36,7 @@ if __name__ == "__main__":
                             dict[line[0]] += len(line[1].strip().split(" "))
                         else:
                             listoflists.remove(line)
-                        result += "\n" + line[0] + ": " + line[1]
+                        result += line[0] + " says: " + line[1] + "\n"
             
             nameList = name.split("HTML/")
             newName = nameList[0] + "scenes.txt"
